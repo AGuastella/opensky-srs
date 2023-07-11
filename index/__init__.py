95db01cc7259aac69eaa4ffa7991a3d88e678a17
Generoso Longo <generosolongo.gl@gmail.com>
2023-07-11 10:41:58 +0200

    Commit prova

--- index/__init__.py
+++ index/__init__.py
@@ -5,3 +5,4 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     f = open(os.path.dirname(os.path.realpath(__file__)) + '/../content/index.html')
     return func.HttpResponse(f.read(), mimetype='text/html')
+#suca
