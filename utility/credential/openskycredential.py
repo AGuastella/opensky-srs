39b715724d0ad2fe2c120596e67c49271ed13989
AGuastella <adriano.guastella.57@gmail.com>
2023-07-14 14:21:00 +0200

    modificate updatedb, ora usa il timestamp locale e non quello della risposta. modificata la richiesta alle api, che ora cicla tra le crednziali disponibili

--- utility/credential/openskycredential.py
+++ utility/credential/openskycredential.py
@@ -1,5 +1,6 @@
 opensky_credential = [
     ['gennibello', '!5pSggwN2!grYTw'],
     ['genelonopensky', 'dkKIskisiKIKI£443'],
+    #['g1g1', 'dkKIskisiKIKI£443'],
     ['Guastella', 'kf7Bw5!R7CJYq8k']
 ]
