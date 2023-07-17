e110078511676cd12dd8bd85465be80cdbec2780
AGuastella <adriano.guastella.57@gmail.com>
2023-07-17 12:06:01 +0200

    second test

---
+++ test_web_connection_g.py
@@ -0,0 +1,9 @@
+import requests
+import pytest
+
+@pytest.mark.parametrize("url", ["https://www.google.com"])
+def test_web_connection(url):
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
