8040392e9c42bc12c371e9192a53bd3444ffd52b
AGuastella <adriano.guastella.57@gmail.com>
2023-07-17 19:22:32 +0200

    added tests

---
+++ test_db_connection.py
@@ -0,0 +1,23 @@
+import pytest
+import pyodbc
+import pandas as pd
+from utility.query_statement import query as q
+
+server = 'openskysrs.database.windows.net'
+database = 'openskydb'
+username = 'CloudSA2b425ff0'
+password = 'colajanni<3'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+@pytest.mark.parametrize("url", ["https://www.google.com"])
+def test_web_connection(url):
+    
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+           data = pd.read_sql(q.select_active_flights,conn).values.tolist()
+    print(data[0])
+
+    assert data[0]
+
+
+#https://openskyiu.azurewebsites.net/api/index?
+
