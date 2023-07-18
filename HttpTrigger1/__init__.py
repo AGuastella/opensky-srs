2e483316ab24e54798960665b2b5d8f2473843c1
Generoso Longo <generosolongo.gl@gmail.com>
2023-07-18 13:26:09 +0200

    Cancellati file vecchi

--- HttpTrigger1/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-
-    with open("/etc/odbcinst.ini") as o:
-        text = ''.join(o.readlines())
-
-    return func.HttpResponse(text)
